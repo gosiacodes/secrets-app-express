9bd257b0cc9eb33a21bbb008b9357d6d14e47f52
Gosia <margareta.xx@gmail.com>
2023-08-25 18:44:27 +0200

    Change encryption into hashing

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //jshint esversion:6
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { default: mongoose } = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 app.use(express.static("public"));
@@ -23,8 +23,6 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
@@ -42,34 +40,34 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
 
   newUser
-      .save()
-      .then(() => {
-        console.log("New user saved successfully! ✅");
-        res.send(newArticle);
-      })
-      .catch((error) => res.send(error));
+    .save()
+    .then(() => {
+      console.log("New user saved successfully! ✅");
+      res.send(newArticle);
+    })
+    .catch((error) => res.send(error));
 });
 
 app.post("/login", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+  const username = req.body.username;
+  const password = md5(req.body.password);
 
-    User.findOne({email: username})
+  User.findOne({ email: username })
     .then((foundUser) => {
-        if (foundUser) {
-          if(foundUser.password === password) {
-            res.render("secrets");
-          }
-        } else {
-          res.send("User not found");
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
         }
-      })
-      .catch((error) => res.send(error));
-})
+      } else {
+        res.send("User not found");
+      }
+    })
+    .catch((error) => res.send(error));
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
